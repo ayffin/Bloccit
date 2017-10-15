c44ba76d4779df1cd1fac2eac3b7e0d0ee2a5ced
aifi <ayffin@gmail.com>
2017-10-15 18:43:19 -0400

    finshed checkpoint 7 assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.where('id == 1'||'id % 5 == 0' ).update_all(title: "Spam")
+
+    # @posts.each_with_index {|index,post|
+    #   if index % 5 ==0
+    #     post.title = "spam"
+    #   end}
   end
 
   def show
