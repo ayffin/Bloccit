72e22bb8e69ad17b0bcb0257d81ed599e9c486c7
aifi <ayffin@gmail.com>
2017-10-25 17:30:02 -0400

    finished checkpoint 14 assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
